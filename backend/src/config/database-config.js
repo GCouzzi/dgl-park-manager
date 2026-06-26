18ffa1b4160153152caff46273694cea25f71951
GCouzzi <gabrielcouzzihb20@gmail.com>
2026-06-25 21:42:03 -0300

    feat: implantacao, novo db

--- backend/src/config/database-config.js
+++ backend/src/config/database-config.js
@@ -28,10 +28,10 @@ export const databaseConfig = {
 
 export const databaseConfig = {
   dialect: 'postgres',
-  host: 'dpg-d8djgqho3t8c73ekfn5g-a.oregon-postgres.render.com',
+  host: 'postgresql://dgl_park_manager_user:0YgJJdC1OVfBk7X6gtfaRaz10fSgScCY@dpg-d8usk8favr4c73fn9n1g-a/dgl_park_manager_ncj1',
   username: 'dgl_park_manager_user',
-  password: 'iLHBwch2jsNlMToi4pMf6WyoJ45BWgSo',
-  database: 'dgl_park_manager',
+  password: '0YgJJdC1OVfBk7X6gtfaRaz10fSgScCY',
+  database: 'dgl_park_manager_ncj1',
   define: {
     timestamps: true,
     freezeTableName: true,
